d93108aa1dda6961da8bc698b6433e353fd93bf9
Gopi Snath <gopinath.mca2010@gmail.com>
2025-07-26 14:40:45 +0530

    fix: added base path for Netlify build

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path'; // ✅ Add this
+import path from 'path';
 
 export default defineConfig({
-  base: './',
+  base: '/', // ✅ optional for Netlify
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'), // ✅ This maps @ to /src
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
