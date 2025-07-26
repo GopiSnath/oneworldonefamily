34b757e49927a019dc486c6a97501cd1103dc435
GopiSnath <134706051+GopiSnath@users.noreply.github.com>
2025-07-26 14:29:40 +0530

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path'; // ✅ Add this
 
 export default defineConfig({
-  base: './',
+  base: '/oneworldonefamily/',
   plugins: [react()],
   resolve: {
     alias: {
