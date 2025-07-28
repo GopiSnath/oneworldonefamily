3db8d1cb1c116c52c90caf377977d717d1197011
Gopi Snath <gopinath.mca2010@gmail.com>
2025-07-28 18:33:33 +0530

    fix: use CommonJS syntax in tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,10 +2,10 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx,html}"
+    "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
