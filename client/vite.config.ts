8a9afb0653b596d856d65de29d905bc0cd981c40
Gopi Snath <gopinath.mca2010@gmail.com>
2025-07-30 18:32:31 +0530

    fix: correct Netlify build path

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  base: './', // ✅ use relative paths for Netlify
+  base: '', // ✅ use relative paths for Netlify
   plugins: [react()],
   resolve: {
     alias: {
